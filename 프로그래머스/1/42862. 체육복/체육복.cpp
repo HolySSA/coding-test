f05fe6c2c1e042835a83c2a44c323125b5d03860
SSA <158470963+shinseongan@users.noreply.github.com>
2024-07-24 15:28:40 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = n - lost.size();
+    
+    for(int i=0; i<reserve.size(); i++){
+        for(int j=0; j<lost.size(); j++){
+            if(reserve[i] == lost[j]){
+                lost.erase(lost.begin()+j);
+                reserve.erase(reserve.begin()+i);
+                i--, j--;
+                answer++;
+            }
+        }
+    }
+    
+    sort(lost.begin(), lost.end());
+    sort(reserve.begin(), reserve.end());
+    
+    for(int i=0; i<reserve.size(); i++){
+        for(int j=0; j<lost.size(); j++){
+            if(reserve[i]-1 == lost[j] || reserve[i]+1 == lost[j]){
+                lost.erase(lost.begin()+j);
+                reserve.erase(reserve.begin()+i);
+                answer++;
+                i--, j--;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
