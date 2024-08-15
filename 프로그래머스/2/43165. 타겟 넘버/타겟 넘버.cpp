e11a1f3aee31c6fcf35e886639493ad2d64bbb1f
SSA <158470963+shinseongan@users.noreply.github.com>
2024-08-15 12:48:15 +0900

    [level 2] Title: 타겟 넘버, Time: 101.57 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int answer = 0;
+bool visited[20];
+
+void DFS(int idx, int sum, vector<int> numbers, int target){
+    if(idx == numbers.size()){
+        if(sum == target)
+            answer++;
+        
+        return;
+    }
+    
+    DFS(idx+1, sum-numbers[idx], numbers, target);
+    DFS(idx+1, sum+numbers[idx], numbers, target);
+}
+
+int solution(vector<int> numbers, int target) {
+    DFS(0, 0, numbers, target);
+    return answer;
+}
