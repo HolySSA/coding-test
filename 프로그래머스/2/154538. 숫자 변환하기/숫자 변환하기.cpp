eab777aa7274932cae9184c970368f8cbcb0baba
SSA <158470963+shinseongan@users.noreply.github.com>
2024-08-21 10:51:20 +0900

    [level 2] Title: 숫자 변환하기, Time: 7.80 ms, Memory: 7.45 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -0,0 +1,31 @@
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+#define MAX 1000001
+
+using namespace std;
+
+int dp[MAX];
+
+int solution(int x, int y, int n) {
+    fill(dp, dp+MAX, MAX);
+
+    dp[y] = 0;
+    
+    for(int i=y; i>x; i--){
+        if(dp[i] != MAX){
+            if(i%3 == 0)
+                dp[i/3] = min(dp[i/3], dp[i]+1);
+            if(i%2 == 0)
+                dp[i/2] = min(dp[i/2], dp[i]+1);
+            if(i-n > 0)
+                dp[i-n] = min(dp[i-n], dp[i]+1);
+        }
+    }
+    
+    if(dp[x] == MAX)
+        dp[x] = -1;
+    
+    return dp[x];
+}
