c3cad6aefcf0e5b931f844ecfb508fe68cf09c14
SSA <158470963+HolySSA@users.noreply.github.com>
2024-11-01 09:16:54 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 120.11 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/README.md
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/README.md
@@ -1,10 +1,10 @@
 # [level 2] 유사 칸토어 비트열 - 148652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148652?language=cpp) 
 
 ### 성능 요약
 
-메모리: 36.1 MB, 시간: 672.04 ms
+메모리: 4.14 MB, 시간: 120.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 01일 09:10:16
+2024년 11월 01일 09:16:51
 
 ### 문제 설명
 
--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.cpp
@@ -6,23 +6,21 @@ using namespace std;
 
 typedef long long ll;
 
-int substitute(ll idx, int level) {
-    if(level == 0)
-        return 1;
+bool findZero(ll num) {
+    if(num < 5 && num != 2)
+        return true;
+    if((num - 2) % 5 == 0)
+        return false;
     
-    ll size = pow(5, level);
-    // 11 0 11 <<< 가운데 부분을 도출 (항상 가운데는 0)
-    if(size / 5 * 2 <= idx && idx < size / 5 * 3)
-        return 0;
-    
-    return substitute(idx % (size / 5), level - 1);
+    return findZero(floor(num / 5));
 }
 
-int solution(int n, long long l, long long r) {
+int solution(int n, ll l, ll r) {
     int answer = 0;
     
     for(ll i = l; i <= r; i++) {
-        answer += substitute(i - 1, n);
+        if(findZero(i - 1))
+            answer++;
     }
     
     return answer;
