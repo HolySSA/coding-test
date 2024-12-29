11547660ae40fa2f6917b84f8c344e8b5311fe95
SSA <158470963+HolySSA@users.noreply.github.com>
2024-12-29 11:12:48 +0900

    Update GCD 합.cc

--- 백준/Silver/9613. GCD 합/GCD 합.cc
+++ 백준/Silver/9613. GCD 합/GCD 합.cc
@@ -1,39 +1,39 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int GCD(int a, int b){
-    if(b==0)
-        return a;
-    else
-        return GCD(b, a%b);
+int t;
+long long sum = 0;
+int num[100];
+
+int GCD(int x, int y) {
+	if (y == 0)
+		return x;
+	else
+		return GCD(y, x % y);
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+int main() {
+	cin >> t;
 	
-	int t; cin>>t;
-    
-	while(t--){
-		int n; cin>>n;
+	while (t--) {
+		memset(num, 0, sizeof(num));
+		sum = 0;
 
-		vector<int> arr;
-		for(int i=0; i<n; i++) {
-			int value; cin>>value;
-			arr.push_back(value);
+		int n; cin >> n;
+		for (int i = 0; i < n; i++) {
+			int tmp; cin >> tmp;
+			num[i] = tmp;
 		}
 
-		long long sum=0;
-        
-		for(int i=0; i<n-1; i++) {
-			for(int j=i+1; j<n; j++) {
-				sum += GCD(arr[i], arr[j]);
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				sum += GCD(num[i], num[j]);
 			}
 		}
-		cout<<sum<<"\n";
+		
+		cout << sum << '\n';
 	}
-    
-    return 0;
+
+	return 0;
 }
