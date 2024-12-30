da3cd522181ed468ae20af3df5bfcb95c97d8949
SSA <158470963+HolySSA@users.noreply.github.com>
2024-12-30 11:06:22 +0900

    Update 30.cc

--- 백준/Silver/10610. 30/30.cc
+++ 백준/Silver/10610. 30/30.cc
@@ -31,36 +31,3 @@ int main() {
 
 	return 0;
 }
-#include<iostream>
-#include<string>
-#include<algorithm>
-using namespace std;
-
-string n;
-
-bool Comp(char a, char b) {
-	return a > b;
-}
-
-int main() {
-	cin >> n;
-
-	sort(n.begin(), n.end(), Comp);
-
-	if (n.back() != '0') {
-		cout << -1;
-		exit(0);
-	}
-
-	long long sum = 0;
-	for (int i = 0; i < n.length(); i++) {
-		sum += (long long)(n[i] - '0');
-	}
-
-	if (sum % 3 == 0)
-		cout << n;
-	else
-		cout << -1;
-
-	return 0;
-}
