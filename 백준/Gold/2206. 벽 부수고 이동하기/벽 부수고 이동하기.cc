89b0210053c58c0c5bacecdf54f487d5dfe14edf
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-02 10:26:59 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 92 ms, Memory: 13508 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -2,7 +2,7 @@
 #include <queue>
 using namespace std;
 
-typedef struct Info {
+struct Info {
     int r;
     int c;
     int wall;
@@ -11,62 +11,62 @@ typedef struct Info {
 int n, m;
 int map[1000][1000];
 int result[1000][1000];
-bool visited[1000][1000][2]; // 벽을 뚫었을 때와 안 뚫었을 때를 따로 체크해야함
+bool visited[1000][1000][2]; // 벽을 뚫었을 때 / 안 뚫었을 때
 
-int dr[] = { 1, -1, 0, 0 };
-int dc[] = { 0, 0, 1, -1 };
+int dr[] = { 1, 0, -1, 0 };
+int dc[] = { 0, 1, 0, -1 };
 
 void Move() {
     queue<Info> q;
     q.push({ 0, 0, 0 });
+    result[0][0] = 1;
     visited[0][0][0] = true;
     visited[0][0][1] = true;
-    result[0][0] = 1;
 
     while (!q.empty()) {
-        int r = q.front().r;
-        int c = q.front().c;
-        int wall = q.front().wall;
+        int cr = q.front().r;
+        int cc = q.front().c;
+        int cwall = q.front().wall;
         q.pop();
 
         for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
+            int nr = cr + dr[i];
+            int nc = cc + dc[i];
 
             if (nr < 0 || nc < 0 || nr >= n || nc >= m)
                 continue;
 
             // 벽이고 부순적이 없으면서 방문안한곳 
-            if (map[nr][nc] == 1 && wall == 0 && !visited[nr][nc][wall + 1]) {
-                visited[nr][nc][wall + 1] = true;
-                result[nr][nc] = result[r][c] + 1;
-                q.push({ nr, nc, wall + 1 });
+            if (map[nr][nc] == 1 && cwall == 0 && !visited[nr][nc][1]) {
+                visited[nr][nc][1] = true;
+                result[nr][nc] = result[cr][cc] + 1;
+                q.push({ nr, nc, 1 });
             }
             //벽이 아니고 방문 안한곳
-            if (map[nr][nc] != 1 && !visited[nr][nc][wall]) {
-                visited[nr][nc][wall] = true;
-                result[nr][nc] = result[r][c] + 1;
-                q.push({ nr, nc, wall });
+            if (map[nr][nc] == 0 && !visited[nr][nc][cwall]) {
+                visited[nr][nc][cwall] = true;
+                result[nr][nc] = result[cr][cc] + 1;
+                q.push({ nr, nc, cwall });
             }
             //목표에 도착했을 경우
             if (nr == n - 1 && nc == m - 1) {
-                cout << result[nr][nc] << '\n';
+                cout << result[nr][nc];
                 return;
             }
         }
     }
 
-    cout << -1 << '\n';
+    cout << -1;
+    return;
 }
 
 int main() {
     cin >> n >> m;
 
     if (n == 1 && m == 1) {
-        cout << 1 << '\n';
+        cout << 1;
         return 0;
     }
-
 
     for (int i = 0; i < n; i++) {
         string str; cin >> str;
@@ -76,6 +76,5 @@ int main() {
     }
 
     Move();
-
     return 0;
 }
