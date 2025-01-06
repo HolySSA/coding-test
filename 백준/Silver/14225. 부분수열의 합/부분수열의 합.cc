4fa1ab559986f722b350821ba4707de641561dc0
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-06 10:54:29 +0900

    [Silver I] Title: 부분수열의 합, Time: 8 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 20000001
+
+int n;
+int S[21];
+bool visited[MAX];
+
+void Program(int idx, int sum){
+    visited[sum] = true;
+    
+    if(idx == n)
+        return;
+    
+    Program(idx+1, sum);
+    Program(idx+1, sum+S[idx]);
+}
+
+int main(){
+    cin>>n;
+    
+    for(int i=0; i<n; i++)
+        cin>>S[i];
+
+    Program(0, 0);
+    
+    int cnt = 1;
+	while(visited[cnt])
+        cnt++;
+    
+	cout<<cnt;
+}
