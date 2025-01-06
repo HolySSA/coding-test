c1153062297fa4f056abbe1a283b7492b26372ec
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-06 10:25:37 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -2,24 +2,23 @@
 #include<string>
 using namespace std;
 
-int n;
-int title = 666, series = 0;
+int n, title = 666, series = 0;
 
 int main() {
 	cin >> n;
 
-	while (series != n) {
+	while (1) {
 		string str = to_string(title);
+
 		for (int i = 0; i < str.length() - 2; i++) {
 			if (str[i] == '6' && str[i + 1] == '6' && str[i + 2] == '6') {
 				series++;
-
-				if (series == n)
-					title--;
-
 				break;
 			}
 		}
+
+		if (series == n)
+			break;
 
 		title++;
 	}
