74296ff9b563f1ec97956f82068cf1413ea23fbb
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-11 10:24:06 +0900

    [Gold IV] Title: 알파벳, Time: 384 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,47 +1,42 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 using namespace std;
 
-int row, col, result;
+int r, c, answer = -1;
 char board[20][20];
-bool alphabet[26];
+bool alphabet[26] = { false, };
 
-int dr[] = { 1, -1, 0, 0 };
-int dc[] = { 0, 0, 1, -1 };
+int dr[] = { 1, 0, -1, 0 };
+int dc[] = { 0, 1, 0, -1 };
 
-void Program(int r, int c, int cnt) {
-	result = max(result, cnt);
+void Move(int cr, int cc, int cnt) {
+	answer = max(answer, cnt);
 
 	for (int i = 0; i < 4; i++) {
-		int nr = r + dr[i];
-		int nc = c + dc[i];
+		int nr = cr + dr[i];
+		int nc = cc + dc[i];
 
-		if (nr < 0 || nc < 0 || nr >= row || nc >= col)
+		if (nr < 0 || nc < 0 || nr >= r || nc >= c)
+			continue;
+		if (alphabet[board[nr][nc] - 'A'])
 			continue;
 
-		int tmp = board[nr][nc] - 'A';
-		if (!alphabet[tmp]) {
-			alphabet[tmp] = true;
-			Program(nr, nc, cnt + 1);
-			alphabet[tmp] = false;
-		}
+		alphabet[board[nr][nc] - 'A'] = true;
+		Move(nr, nc, cnt + 1);
+		alphabet[board[nr][nc] - 'A'] = false;
 	}
 }
 
 int main() {
-	cin >> row >> col;
-
-	for (int i = 0; i < row; i++) {
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
 		string str; cin >> str;
-
-		for (int j = 0; j < col; j++)
+		for (int j = 0; j < c; j++) {
 			board[i][j] = str[j];
+		}
 	}
 
-	int start = board[0][0] - 'A';
-	alphabet[start] = true;
-	Program(0, 0, 1);
-	cout << result;
-
+	alphabet[board[0][0] - 'A'] = true;
+	Move(0, 0, 1);
+	cout << answer;
 	return 0;
 }
