2050db981bff214a4f7e9fbb6a6b0ec64d08409c
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-12 11:35:35 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,30 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int n;
-int T[15], P[15];
-int res[15];
-
-void Dp(){
-	int sum;
-	for(int i=n-1; i>=0; i--){
-		sum = i+T[i];
-		if(sum > n){
-			res[i] = res[i+1];
-		}
-		else {
-			res[i] = max(res[i+1], res[sum]+P[i]);
-		}
-	}
-}
+int money[16];
+vector<pair<int, int>> consult;
 
 int main() {
-    cin>>n;
-	for (int i=0; i<n; i++)
-		cin>>T[i]>>P[i];
-    
-	Dp();
-	
-	cout <<res[0]<<'\n';
+	cin >> n;
+	consult.resize(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> consult[i].first >> consult[i].second;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + consult[i].first; j <= n; j++) {
+			money[j] = max(money[j], consult[i].second + money[i]);
+		}
+	}
+
+	cout << money[n];
 	return 0;
 }
