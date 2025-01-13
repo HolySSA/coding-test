d7029e8d17e8e1f30a671d6b3715786f70e0e706
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-13 11:06:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 104 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<pair<int, int>> pos;
+
+bool Comp(pair<int, int> a, pair<int, int> b) {
+	if (a.first == b.first)
+		return a.second < b.second;
+
+	return a.first < b.first;
+}
+
+int main() {
+	cin >> n;
+	pos.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> pos[i].first >> pos[i].second;
+	}
+
+	sort(pos.begin(), pos.end(), Comp);
+
+	for (auto p : pos) {
+		cout << p.first << ' ' << p.second << '\n';
+	}
+	return 0;
+}
