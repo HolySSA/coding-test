9d4823618b12c72d901f064c723c73c22bd70fc3
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-14 11:19:51 +0900

    [Silver IV] Title: 카드, Time: 28 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.cc
+++ 백준/Silver/11652. 카드/카드.cc
@@ -1,42 +1,34 @@
-// 11652 카드
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+typedef long long ll;
+
 int n;
-vector<long long> vec;
+map<ll, int> m;
+vector<pair<ll, int>> v;
+
+bool Comp(pair<ll, int> a, pair<ll, int> b) {
+	if (a.second == b.second)
+		return a.first < b.first;
+
+	return a.second > b.second;
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
 	cin >> n;
-	vec.resize(n);
-	for (int i = 0; i < n; i++)
-		cin >> vec[i];
-
-	sort(vec.begin(), vec.end());
-
-	long long result = vec[0];
-	int max = 0, cnt = 0;
-	for (int i = 1; i < n; i++)
-	{
-		// 정렬했으므로
-		if (vec[i] == vec[i - 1])
-		{
-			cnt++;
-			if (cnt > max)
-			{
-				max = cnt;
-				result = vec[i];
-			}
-		}
-		else
-			cnt = 0;
+	for (int i = 0; i < n; i++) {
+		ll num; cin >> num;
+		m[num]++;
 	}
 
-	cout << result;
+	for (auto iter : m) {
+		v.push_back({ iter.first, iter.second });
+	}
 
+	sort(v.begin(), v.end(), Comp);
+	cout << v[0].first;
 	return 0;
 }
