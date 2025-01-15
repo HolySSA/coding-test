78758474184e4786cec4ca0a018415d62666c0eb
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-15 11:04:59 +0900

    [Silver I] Title: 기타 레슨, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> video;
+
+bool canDivide(int maxSize) {
+	int cnt = 1, sum = 0;
+	for (int length : video) {
+		if (sum + length > maxSize) {
+			cnt++;
+			sum = 0;
+		}
+
+		sum += length;
+		if (length > maxSize)
+			return false;
+	}
+
+	return cnt <= m;
+}
+
+int main() {
+	cin >> n >> m;
+	video.resize(n);
+
+	int low = 0, high = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> video[i];
+		low = max(low, video[i]);\
+		high += video[i];\
+	}
+
+	int answer = high;
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (canDivide(mid)) {
+			answer = mid;
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1;
+		}
+	}
+
+	cout << answer;
+	return 0;
+}
