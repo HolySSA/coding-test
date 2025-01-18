41234bda523f0daf523a33be42f7abe2368a41b6
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-18 11:08:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 316 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,30 +1,22 @@
 #include <iostream>
-#include <algorithm>
-
+#include <unordered_map>
 using namespace std;
 
-int arr[500002];
-int N, M, card;
+int n, m;
+unordered_map<int, int> cards;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-    
-	cin >> N;
-	for (int i=0; i<N; i++) {
-		cin >> card;
-		arr[i] = card;
+	cin >> n;
+	while (n--) {
+		int num; cin >> num;
+		cards[num]++;
 	}
-    
-	sort(arr, arr + N); // 오름차순 정렬
 
-	cin >> M;
-	for (int i = 0; i < M; i++) {
-		cin >> card;
-        //이분 탐색
-        //lower_bound(,,s)의 결과 : s 이상의 값이 처음 나타나는 위치
-        //upper_bound(,,s)의 결과 : s를 초과하는 값이 처음 나타나는 위치
-        //따라서 오름차순으로 정렬된 arr에서 card값이 입력되면 갯수가 나오게 된다
-		cout << upper_bound(arr, arr + N, card) - lower_bound(arr, arr + N, card)<<" ";
+	cin >> m;
+	while (m--) {
+		int num; cin >> num;
+		cout << cards[num] << ' ';
 	}
+
+	return 0;
 }
