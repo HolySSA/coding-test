7a5b663a7884dab9fdecbce9f3ce36421f813b50
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-21 10:53:53 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,26 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int n, s, result = 0;
-int num[20];
+int n, s, answer = 0;
+vector<int> sums;
 
-void Plus(int idx, int sum) {
+void Calculate(int idx, int sum) {
 	if (idx == n)
 		return;
-	if (sum + num[idx] == s)
-		result++;
+	if (sum + sums[idx] == s)
+		answer++;
 
-	Plus(idx + 1, sum);
-	Plus(idx + 1, sum + num[idx]);
+	Calculate(idx + 1, sum);
+	Calculate(idx + 1, sum + sums[idx]);
 }
 
 int main() {
 	cin >> n >> s;
-	for (int i = 0; i < n; i++)
-		cin >> num[i];
+	sums.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> sums[i];
+	}
 
-	Plus(0, 0);
-
-	cout << result;
+	Calculate(0, 0);
+	cout << answer;
 	return 0;
 }
