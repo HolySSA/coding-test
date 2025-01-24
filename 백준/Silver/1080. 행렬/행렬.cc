b43e58886b17c4f873173052ea76cc77b36d4085
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-24 10:56:21 +0900

    [Silver I] Title: 행렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.cc
+++ 백준/Silver/1080. 행렬/행렬.cc
@@ -1,65 +1,50 @@
-// 1080 행렬
 #include <iostream>
 using namespace std;
 
-int N, M, cnt = 0;
-char matrix1[50][50];
-char matrix2[50][50];
+int n, m, answer = 0;
+int a[50][50], b[50][50];
 
-void reverse(int x, int y)
-{
-	for (int i = x; i < x + 3; ++i)
-	{
-		for (int j = y; j < y + 3; ++j)
-		{
-			if (matrix1[i][j] == '1')
-			{
-				matrix1[i][j] = '0';
-			}
-			else
-			{
-				matrix1[i][j] = '1';
-			}
+void Reverse(int r, int c) {
+	for (int i = r; i < r + 3; i++) {
+		for (int j = c; j < c + 3; j++) {
+			a[i][j] = a[i][j] == 0 ? 1 : 0;
 		}
 	}
 }
 
-int main()
-{
-	ios::sync_with_stdio(false); cin.tie(0);
-	cin >> N >> M;
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		string str; cin >> str;
+		for (int j = 0; j < m; j++) {
+			a[i][j] = str[j] - '0';
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		string str; cin >> str;
+		for (int j = 0; j < m; j++) {
+			b[i][j] = str[j] - '0';
+		}
+	}
 
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < M; ++j)
-			cin >> matrix1[i][j];
-
-	for (int i = 0; i < N; ++i)
-		for (int j = 0; j < M; ++j)
-			cin >> matrix2[i][j];
-
-	for (int i = 0; i < N - 2; ++i)
-	{
-		for (int j = 0; j < M - 2; ++j)
-		{
-			if (matrix1[i][j] != matrix2[i][j])
-			{
-				reverse(i, j);
-				cnt++;
+	for (int i = 0; i < n - 2; i++) {
+		for (int j = 0; j < m - 2; j++) {
+			if (a[i][j] != b[i][j]) {
+				Reverse(i, j);
+				answer++;
 			}
 		}
 	}
 
-	bool flag = false;
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < M; ++j)
-		{
-			if (matrix1[i][j] != matrix2[i][j])
-				flag = true;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (a[i][j] != b[i][j]) {
+				cout << -1;
+				return 0;
+			}
 		}
 	}
 
-	if (flag) cout << -1;
-	else cout << cnt;
+	cout << answer;
 	return 0;
 }
