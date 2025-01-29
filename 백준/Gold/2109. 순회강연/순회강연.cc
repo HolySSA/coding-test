98492365cc7136916d6dcf2beb6b2ea0850f18f1
SSA <158470963+HolySSA@users.noreply.github.com>
2025-01-29 11:07:48 +0900

    [Gold III] Title: 순회강연, Time: 8 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 int n, answer = 0;
 vector<pair<int, int>> lecture;
+priority_queue<int, vector<int>, greater<int>> pq;
 
 bool Comp(pair<int, int> a, pair<int, int> b) {
     return a.second < b.second;
@@ -14,20 +15,17 @@ bool Comp(pair<int, int> a, pair<int, int> b) {
 int main() {
     cin >> n;
     lecture.resize(n);
-    
+
     for (int i = 0; i < n; i++) {
         cin >> lecture[i].first >> lecture[i].second;
     }
 
     sort(lecture.begin(), lecture.end(), Comp);
 
-    priority_queue<int, vector<int>, greater<int>> pq;
-
-    for (auto &lec : lecture) {
+    for (auto& lec : lecture) {
         pq.push(lec.first);
-        if (pq.size() > lec.second) {
+        if (pq.size() > lec.second)
             pq.pop();
-        }
     }
 
     while (!pq.empty()) {
