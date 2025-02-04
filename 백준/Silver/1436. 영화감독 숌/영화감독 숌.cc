caf246eb28eb061a40fc9b0b364113156e699d8c
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-04 10:16:41 +0900

    [Silver V] Title: 영화감독 숌, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,15 +1,13 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int n, title = 666, series = 0;
 
 int main() {
 	cin >> n;
-
 	while (1) {
 		string str = to_string(title);
-
 		for (int i = 0; i < str.length() - 2; i++) {
 			if (str[i] == '6' && str[i + 1] == '6' && str[i + 2] == '6') {
 				series++;
@@ -17,13 +15,12 @@ int main() {
 			}
 		}
 
-		if (series == n)
+		if (n == series)
 			break;
 
 		title++;
 	}
 
 	cout << title;
-
 	return 0;
 }
