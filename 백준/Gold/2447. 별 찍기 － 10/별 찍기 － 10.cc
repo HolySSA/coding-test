29cbc1f6e33ec96fb32adef95eaafd99f6c82359
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-07 10:41:37 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 264 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+void Star(int r, int c, int num) {
+	if ((r / num) % 3 == 1 && (c / num) % 3 == 1)
+		cout << ' ';
+	else if ((num / 3 == 0))
+		cout << '*';
+	else
+		Star(r, c, num / 3);
+
+}
+
+int main() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			Star(i, j, n);
+		}
+		cout << '\n';
+	}
+	return 0;
+}
