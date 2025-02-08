832802dca36ca0caaaf8b19ed350af0e6382e40a
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-08 10:01:00 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.cc
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -19,30 +19,30 @@ int main() {
 
 	sort(nums.begin(), nums.end());
 
-    // 음수
-    int i = 0;
-    while (i < n - 1 && nums[i] <= 0 && nums[i + 1] <= 0) {
-        answer += nums[i] * nums[i + 1];
-        i += 2;
-    }
-
-    while (i < n && nums[i] <= 0) {
-        answer += nums[i];
-        i++;
-    }
-
-    // 양수
-    int j = n - 1;
-    while (j > 0 && nums[j] > 1 && nums[j - 1] > 1) {
-        answer += nums[j] * nums[j - 1];
-        j -= 2;
-    }
-
-    while (j >= 0 && nums[j] > 0) {
-        answer += nums[j];
-        j--;
-    }
-
-    cout << answer;
-    return 0;
+	// 음수
+	int i = 0;
+	while (i < n - 1 && nums[i] <= 0 && nums[i + 1] <= 0) {
+	answer += nums[i] * nums[i + 1];
+	i += 2;
+	}
+	
+	while (i < n && nums[i] <= 0) {
+	answer += nums[i];
+	i++;
+	}
+	
+	// 양수
+	int j = n - 1;
+	while (j > 0 && nums[j] > 1 && nums[j - 1] > 1) {
+	answer += nums[j] * nums[j - 1];
+	j -= 2;
+	}
+	
+	while (j >= 0 && nums[j] > 0) {
+	answer += nums[j];
+	j--;
+	}
+	
+	cout << answer;
+	return 0;
 }
