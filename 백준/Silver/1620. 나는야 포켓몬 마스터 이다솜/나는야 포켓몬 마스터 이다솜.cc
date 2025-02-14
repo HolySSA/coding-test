44dc41ba5afc4dad6821c07df289a61d838fda97
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-14 10:20:59 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 216 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,34 +1,34 @@
-#include<iostream>
-#include<map>
-#include<string>
+#include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
-map<int, string> monster;
-map<string, int> poket;
+int n, m;
+string name;
+map<int, string> pokemon;
+map<string, int> number;
 
-int main(){
-    cin.tie(0); cout.tie(0);
+int main() {
+	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(0);
-    
-    int n,m;
-    cin>>n>>m;
-    
-    for(int i=1; i<=n; i++){
-        string str; cin>>str;
-        poket[str] = i;
-        monster[i] = str;
-    }
-    
-    while(m--){
-        string str; cin>>str;
-        
-        if(str[0] < 'A'){
-            cout<<monster[stoi(str)]<<'\n';
-            continue;
-        }
-         
-        cout<<poket[str]<<'\n';
-    }
-    
-    return 0;
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> name;
+		pokemon[i] = name;
+		number[name] = i;
+	}
+
+	while (m--) {
+		cin >> name;
+		if (isdigit(name[0])) {
+			int num = stoi(name);
+			cout << pokemon[num] << '\n';
+		}
+		else {
+			cout << number[name] << '\n';
+		}
+	}
+
+	return 0;
 }
