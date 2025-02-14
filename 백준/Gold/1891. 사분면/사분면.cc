64c494d883ea8beaef1ca37b38a4acdf93e3d68d
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-14 10:05:50 +0900

    [Gold IV] Title: 사분면, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1891. 사분면/사분면.cc
+++ 백준/Gold/1891. 사분면/사분면.cc
@@ -1,70 +1,77 @@
-// 1891 사분면
 #include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
 
 int d;
-long long x, y;
-string q_Num;
+long long x, y, r = 0, c = 0;
+string str, answer = "";
 
-// 사분면 조각의 번호가 저장되어 있는 문자열을 하나하나씩 검사하여 좌표를 얻는 함수
-pair<long long, long long> get_Coordinate(string& str, int index, long long r, long long c, long long size)
-{
-    if (size == 1)
-        return make_pair(r, c);
-    else
-    {
-        if (str[index] == '1') {
-            return get_Coordinate(str, index + 1, r, c + size / 2, size / 2);
-        }
-        else if (str[index] == '2') {
-            return get_Coordinate(str, index + 1, r, c, size / 2);
-        }
-        else if (str[index] == '3') {
-            return get_Coordinate(str, index + 1, r + size / 2, c, size / 2);
-        }
-        else if (str[index] == '4') {
-            return get_Coordinate(str, index + 1, r + size / 2, c + size / 2, size / 2);
-        }
-    }
+void Coordinate(long long size) {
+	long long half = size / 2;
 
-    return make_pair(0, 0);
+	for (char cur : str) {
+		int quadrant = cur - '0';
+
+		if (quadrant == 1) {
+			r += half;
+			c += half;
+		}
+		else if (quadrant == 2) {
+			r += half;
+		}
+		else if (quadrant == 3) {
+
+		}
+		else if (quadrant == 4) {
+			c += half;
+		}
+
+		half /= 2;
+	}
 }
-string go(long long r, long long c, long long size, long long x, long long y)
-{
-    if (size == 1)
-        return "";
-    if (x < r + size / 2 && y < c + size / 2)
-        return "2" + go(r, c, size / 2, x, y);
-    else if (x < r + size / 2 && y >= c + size / 2)
-        return "1" + go(r, c + size / 2, size / 2, x, y);
-    else if (x >= r + size / 2 && y < c + size / 2)
-        return "3" + go(r + size / 2, c, size / 2, x, y);
-    else
-        return "4" + go(r + size / 2, c + size / 2, size / 2, x, y);
+
+void Quadrant(long long size) {
+	long long half = size / 2;
+
+	while (half > 0) {
+		if (r >= half && c >= half) {
+			answer += '1';
+			r -= half;
+			c -= half;
+		}
+		else if (r >= half && c < half) {
+			answer += '2';
+			r -= half;
+		}
+		else if (r < half && c < half) {
+			answer += '3';
+		}
+		else {
+			answer += '4';
+			c -= half;
+		}
+
+		half /= 2;
+	}
 }
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
 
-    // d ; 사분면 조각 번호의 자릿수, q_Num : 사분면의 조각 번호
-    cin >> d >> q_Num;
-    cin >> x >> y;
-    // shift로 2의 제곱수를 사이즈로
-    long long size = (1LL << d);
+int main() {
+	cin >> d >> str;
+	cin >> x >> y;
 
-    // 사분면 조각의 번호가 해당하는 좌표를 얻는다.
-    pair<long long, long long> p = get_Coordinate(q_Num, 0, 0, 0, size);
+	long long size = (long long)pow(2, d);
+	Coordinate(size);
 
-    // 좌표 이동
-    p.first -= y;
-    p.second += x;
+	//cout << r << ' ' << c << '\n';
+	r += y;
+	c += x;
+	//cout << r << ' ' << c << '\n';
+	if (r < 0 || c < 0 || r >= size || c >= size) {
+		cout << -1;
+		return 0;
+	}
 
-    if (0 <= p.first && p.first < size && 0 <= p.second && p.second < size)
-        cout << go(0, 0, size, p.first, p.second);
-    else
-        cout << "-1" << "\n"; // 좌표가 범위 안에 존재하지 않으면 -1 출력
-
-    return 0;
+	Quadrant(size);
+	cout << answer;
+	return 0;
 }
