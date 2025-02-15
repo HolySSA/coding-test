ec0631dff0e40517d38d3b600c27e4c9851a8dbb
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-15 10:17:29 +0900

    [Silver IV] Title: 듣보잡, Time: 24 ms, Memory: 6172 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,39 +1,28 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <set>
 using namespace std;
 
+int n, m;
+string name;
+set<string> names;
+set<string> answer;
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    
-    vector<string> v,vt;
-    int n, m;
-    cin >> n >> m;
-    
-    string s;
-    
-    for(int i=0; i<n; i++) {
-        cin>>s;
-        v.push_back(s);
-    }
-    
-    sort(v.begin(), v.end());
-    
-    for(int i=0; i<m; i++) {
-        cin>>s;
-        if(binary_search(v.begin(), v.end(), s)) { // 찾는 s가 있으면 1(true) 없으면 0(false)
-            vt.push_back(s);
-        }
-    }
-    
-    sort(vt.begin(), vt.end());
-    
-    cout << vt.size() << "\n";
-    
-    for (auto o : vt) {
-        cout << o << "\n";
-    }
-    
-    return 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		names.insert(name);
+	}
+
+	for (int i = 0; i < m; i++) {
+		cin >> name;
+		if (names.find(name) != names.end())
+			answer.insert(name);
+	}
+
+	cout << answer.size() << '\n';
+	for (auto& n : answer) {
+		cout << n << '\n';
+	}
+	return 0;
 }
