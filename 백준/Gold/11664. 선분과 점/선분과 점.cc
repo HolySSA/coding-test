e0bc2c96a6beb3f6b35c13530d1378cc6143b317
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-20 10:53:27 +0900

    [Gold V] Title: 선분과 점, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/11664. 선분과 점/선분과 점.cc
+++ 백준/Gold/11664. 선분과 점/선분과 점.cc
@@ -1,9 +1,6 @@
-// 11664 선분과 점
 #include<iostream>
 #include<cmath>
 using namespace std;
-
-// 삼분 탐색
 
 struct Pos {
 	double x, y, z;
@@ -11,38 +8,38 @@ struct Pos {
 
 Pos A, B, C;
 
-double Distance(double x1, double y1, double z1, double x2, double y2, double z2) {
-	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2));
+double Distance(Pos a, Pos b) {
+	return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2) + pow(b.z - a.z, 2));
 }
 
 int main() {
 	cin >> A.x >> A.y >> A.z >> B.x >> B.y >> B.z >> C.x >> C.y >> C.z;
 
+	// 삼분 탐색
 	double low = 0, high = 1;
 	while (high - low > 0.0000001) {
+		double mid1 = (2 * low + high) / 3;
+		double mid2 = (low + 2 * high) / 3;
+
 		Pos Mid1, Mid2;
-		double ref1 = (2 * low + high) / 3, ref2 = (low + 2 * high) / 3;
+		Mid1.x = B.x * mid1 + A.x * (1 - mid1), Mid2.x = B.x * mid2 + A.x * (1 - mid2);
+		Mid1.y = B.y * mid1 + A.y * (1 - mid1), Mid2.y = B.y * mid2 + A.y * (1 - mid2);
+		Mid1.z = B.z * mid1 + A.z * (1 - mid1), Mid2.z = B.z * mid2 + A.z * (1 - mid2);
 
-		Mid1.x = B.x * ref1 + A.x * (1 - ref1), Mid2.x = B.x * ref2 + A.x * (1 - ref2);
-		Mid1.y = B.y * ref1 + A.y * (1 - ref1), Mid2.y = B.y * ref2 + A.y * (1 - ref2);
-		Mid1.z = B.z * ref1 + A.z * (1 - ref1), Mid2.z = B.z * ref2 + A.z * (1 - ref2);
-
-		double dist1 = Distance(C.x, C.y, C.z, Mid1.x, Mid1.y, Mid1.z);
-		double dist2 = Distance(C.x, C.y, C.z, Mid2.x, Mid2.y, Mid2.z);
+		double dist1 = Distance(C, Mid1);
+		double dist2 = Distance(C, Mid2);
 
 		if (dist1 > dist2)
-			low = ref1;
+			low = mid1;
 		else
-			high = ref2;
+			high = mid2;
 	}
 
 	Pos Ans;
 	Ans.x = B.x * low + A.x * (1 - low);
 	Ans.y = B.y * low + A.y * (1 - low);
 	Ans.z = B.z * low + A.z * (1 - low);
-
 	cout << fixed; cout.precision(20);
-	cout << Distance(Ans.x, Ans.y, Ans.z, C.x, C.y, C.z) << '\n';
-
+	cout << Distance(Ans, C);
 	return 0;
 }
