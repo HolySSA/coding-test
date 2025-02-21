eff24f50a513fa9c858b4422840158a0e37e9d87
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-21 10:34:22 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int dp[31];
+
+int main() {
+	cin >> n;
+	if (n % 2 != 0) {
+		cout << 0;
+		return 0;
+	}
+
+	dp[0] = 1, dp[2] = 3;
+	for (int i = 2; i <= n; i += 2) {
+		dp[i] = dp[i - 2] * dp[2]; // 3*(i-2)까지 채운 후 3*2 타일 추가
+
+		for (int j = 4; j <= i; j += 2) {
+			dp[i] += dp[i - j] * 2;
+		}
+	}
+
+	cout << dp[n];
+	return 0;
+}
