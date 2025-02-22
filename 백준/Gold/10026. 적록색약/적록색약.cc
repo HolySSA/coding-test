54457b8345c733537aaa65ec609d232bb5116552
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-22 10:51:43 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,100 +1,75 @@
-// 10026 적록색약
-#include<iostream>
-#include<cstring>
-#include<queue>
+#include <iostream>
+#include <queue>
+#include <cstring>
 using namespace std;
 
-struct Pos {
-    int r, c;
-};
-
-int n, ans1 = 0, ans2 = 0;
-char map[100][100];
+int n, normal = 0, blind = 0;
+char board[100][100];
 bool visited[100][100];
 
-int dr[] = { 1, -1, 0, 0 };
-int dc[] = { 0, 0, -1, 1 };
+int dr[] = { 1, 0, -1, 0 };
+int dc[] = { 0, 1, 0, -1 };
 
-void Area(int r, int c)
-{
-    queue<Pos> q;
-    q.push({ r, c });
-    visited[r][c] = true;
+void Area(int r, int c) {
+	queue<pair<int, int>> q;
+	q.push({ r, c });
+	visited[r][c] = true;
 
-    while (!q.empty())
-    {
-        auto cur = q.front();
-        q.pop();
+	while (!q.empty()) {
+		auto cur = q.front();
+		q.pop();
 
-        for (int i = 0; i < 4; i++)
-        {
-            int nr = cur.r + dr[i];
-            int nc = cur.c + dc[i];
+		for (int i = 0; i < 4; i++) {
+			int nr = cur.first + dr[i];
+			int nc = cur.second + dc[i];
 
-            if (nr < 0 || nc < 0 || nr >= n || nc >= n)
-                continue;
-            if (visited[nr][nc])
-                continue;
-            
-            if (map[nr][nc] == map[cur.r][cur.c])
-            {
-                q.push({ nr, nc });
-                visited[nr][nc] = true;
-            }         
-        }
-    }
+			if (nr < 0 || nc < 0 || nr >= n || nc >= n)
+				continue;
+			if (visited[nr][nc])
+				continue;
+
+			if (board[cur.first][cur.second] == board[nr][nc]) {
+				q.push({ nr, nc });
+				visited[nr][nc] = true;
+			}
+		}
+	}
 }
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+		}
+	}
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> map[i][j];
-        }
-    }
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j]) {
+				Area(i, j);
+				normal++;
+			}
+		}
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (!visited[i][j])
-            {
-                Area(i, j);
-                ans1++;
-            }
-        }
-    }
+	memset(visited, false, sizeof(visited));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] == 'G')
+				board[i][j] = 'R';
+		}
+	}
 
-    memset(visited, false, sizeof(visited));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j]) {
+				Area(i, j);
+				blind++;
+			}
+		}
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (map[i][j] == 'R')
-                map[i][j] = 'G';
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (!visited[i][j])
-            {
-                Area(i, j);
-                ans2++;
-            }
-        }
-    }
-
-    cout << ans1 << ' ' << ans2 << '\n';
-
-    return 0;
+	cout << normal << ' ' << blind;
+	return 0;
 }
