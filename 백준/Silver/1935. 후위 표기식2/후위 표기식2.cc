f8a204b93f47d8db2858dc4fccd0329fa9c9f6ac
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-24 10:41:15 +0900

    [Silver III] Title: 후위 표기식2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int n;
+string str;
+double alphabet[26];
+stack<double> s;
+
+int main() {
+	cin >> n >> str;
+	for (int i = 0; i < n; i++) {
+        cin >> alphabet[i];
+	}
+
+	for (int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+        if (ch >= 'A' && ch <= 'Z')
+            s.push(alphabet[ch - 'A']);
+        else {
+            double num2 = s.top();
+            s.pop();
+            double num1 = s.top();
+            s.pop();
+
+            double tmp;
+            if (ch == '+')
+                tmp = num1 + num2;
+            else if (ch == '-')
+                tmp = num1 - num2;
+            else if (ch == '*')
+                tmp = num1 * num2;
+            else if (ch == '/')
+                tmp = num1 / num2;
+                
+            s.push(tmp);
+        }
+	}
+
+    cout.precision(2);
+    cout << fixed << s.top();
+	return 0;
+}
