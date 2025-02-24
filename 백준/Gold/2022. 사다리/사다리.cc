3f1382716831e97ffae27d60c0e1f787ca90d8d1
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-24 10:22:51 +0900

    [Gold IV] Title: 사다리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2022. 사다리/사다리.cc
+++ 백준/Gold/2022. 사다리/사다리.cc
@@ -4,23 +4,21 @@ using namespace std;
 
 double x, y, c;
 
-double Prediction (double distance) {
-	double h1 = sqrt(x * x - distance * distance);
-	double h2 = sqrt(y * y - distance * distance);
-	
-	return (h1 * h2) / (h1 + h2); // c 예측
+double Calculate(double mid) {
+	double h1 = sqrt(x * x - mid * mid);
+	double h2 = sqrt(y * y - mid * mid);
+	return (h1 * h2) / (h1 + h2);
 }
 
 int main() {
 	cin >> x >> y >> c;
 
-	double low = 0;
-	double high = min(x, y); // 어자피 ?는 x, y보다 작다
+	double low = 0, high = min(x, y);
+	while (high - low > 1e-3) {
+		double mid = (low + high) / 2;
+		double height = Calculate(mid);
 
-	while (high - low > 0.001) {
-		double mid = (low + high) / 2.0;
-
-		if (Prediction(mid) >= c)
+		if (c < height)
 			low = mid;
 		else
 			high = mid;
