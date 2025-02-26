30d0cbebf89fcb3c23d3d1ba8dd207f84eba52fc
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-26 10:37:49 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,49 +1,57 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int l, c;
-char input[15], password[15];
-bool visited[15];
+vector<char> ch;
+vector<char> password;
+bool visited[26];
 
-void Password(int cnt, int idx){
-    if(cnt == l){
-        int vowel=0, consonant=0;
-        for(int i=0;i<l;i++){
-            if(password[i] == 'a' || password[i] == 'i' || password[i] == 'u' ||
-               password[i] == 'e' || password[i] == 'o')
-                vowel++;
-            else
-                consonant++;
-        }
-        
-        if(vowel>=1 && consonant>=2){
-            for(int i=0;i<l;i++)
-                cout<<password[i];
-            cout<<'\n';
-        }
-        return;
-    }
-    
-    for(int i=idx;i<c;i++){
-        if(!visited[i]){
-            visited[i] = true;
-            password[cnt] = input[i];
-            Password(cnt+1, i);
-            visited[i] = false;
-        }
-    }
+bool Vowel(char ch) {
+	return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
 }
 
-int main(){
-    cin>>l>>c;
-    
-    for(int i=0;i<c;i++)
-        cin>>input[i];
-    
-    sort(input, input+c);
-    
-    Password(0, 0);
-    
-    return 0;
+void Password(int cnt, int idx) {
+	if (cnt == l) {
+		int vowel = 0, consonant = 0;
+
+		for (char p : password) {
+			if (Vowel(p))
+				vowel++;
+			else
+				consonant++;
+		}
+
+		if (vowel >= 1 && consonant >= 2) {
+			for (char p : password) {
+				cout << p;
+			}
+			cout << '\n';
+		}
+
+		return;
+	}
+
+	for (int i = idx; i < c; i++) {
+		if (!visited[ch[i] - 'a']) {
+			visited[ch[i] - 'a'] = true;
+			password.push_back(ch[i]);
+			Password(cnt + 1, i);
+			password.pop_back();
+			visited[ch[i] - 'a'] = false;
+		}
+	}
+}
+
+int main() {
+	cin >> l >> c;
+	ch.resize(c);
+	for (int i = 0; i < c; i++) {
+		cin >> ch[i];
+	}
+
+	sort(ch.begin(), ch.end());
+	Password(0, 0);
+	return 0;
 }
