9299134185bc850d411c17f1bfdf89f9c3b756b2
SSA <158470963+HolySSA@users.noreply.github.com>
2025-02-28 10:40:30 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> dp;
+
+int main(){
+    int n; cin>>n;
+    
+    dp.push_back(1);
+	dp.push_back(2);
+    
+	for (int i=2; i<n; i++) {
+		dp.push_back((dp[i - 1] + dp[i - 2])%10007);
+	}
+    
+	cout << dp[n - 1] ;
+
+    return 0;
+}
