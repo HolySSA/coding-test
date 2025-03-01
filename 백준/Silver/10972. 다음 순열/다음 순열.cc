69a76f9fc2788d1941c55ecac7620d24f39f5e76
SSA <158470963+HolySSA@users.noreply.github.com>
2025-03-01 10:29:46 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.cc
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -1,28 +1,41 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-#define MAX 10000
+int n, idx = -1;
+vector<int> v;
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+int main() {
+	cin >> n;
+	v.resize(n);
 
-	int num[MAX];
-
-	int n; cin>>n;
-	for (int i=0; i<n; i++)
-		cin>>num[i];
-
-	if(next_permutation(num, num+n))
-	{
-		for(int i=0; i<n; i++)
-			cout<<num[i]<<' ';
-		cout<<'\n';
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
 	}
-	else
-		cout<<-1;
 
+	for (int i = n - 1; i >= 1; i--) {
+		if (v[i] > v[i - 1]) {
+			idx = i - 1;
+			break;
+		}
+	}
+
+	if (idx == -1) {
+		cout << -1;
+		return 0;
+	}
+
+	int swapIdx = n - 1;
+	while (v[swapIdx] <= v[idx]) {
+		swapIdx--;
+	}
+
+	swap(v[idx], v[swapIdx]);
+	reverse(v.begin() + idx + 1, v.end());
+
+	for (auto i : v) {
+		cout << i << ' ';
+	}
 	return 0;
 }
