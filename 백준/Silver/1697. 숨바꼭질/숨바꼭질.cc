f9933943996963546ea5adbc671390a34e50bc5d
SSA <158470963+HolySSA@users.noreply.github.com>
2025-03-09 10:22:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define MAX 100000
+
+int n, k;
+bool visited[MAX + 1];
+
+int HideAndSeek() {
+	queue<pair<int, int>> q;
+	q.push({ n, 0 });
+	visited[n] = true;
+
+	while (!q.empty()) {
+		int cur = q.front().first;
+		int cnt = q.front().second;
+		q.pop();
+
+		if (cur == k)
+			return cnt;
+
+		if (cur - 1 >= 0 && !visited[cur - 1]) {
+			q.push({ cur - 1, cnt + 1 });
+			visited[cur - 1] = true;
+		}
+		if (cur + 1 <= MAX && !visited[cur + 1]) {
+			q.push({ cur + 1, cnt + 1 });
+			visited[cur + 1] = true;
+		}
+		if (2 * cur <= MAX && !visited[2 * cur]) {
+			q.push({ 2 * cur, cnt + 1 });
+			visited[2 * cur] = true;
+		}
+	}
+}
+
+int main() {
+	cin >> n >> k;
+
+	cout << HideAndSeek();
+	return 0;
+}
