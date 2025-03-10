c197e92871a7d34b624b6110d45d92f6b7354c7c
SSA <158470963+HolySSA@users.noreply.github.com>
2025-03-10 09:56:29 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 152 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,26 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int n;
+vector<pair<int, int>> records;
 
-void Move(int n, int startRod, int endRod, int bypass)
-{
-    if (n == 1)
-        cout << startRod << ' ' << endRod << '\n';
-    else
-    {
-        Move(n - 1, startRod, bypass, endRod);
-        cout << startRod << ' ' << endRod << '\n';
-        Move(n - 1, bypass, endRod, startRod);
-    }
+void Hanoi(int from, int mid, int to, int n) {
+	if (n == 1) {
+		records.push_back({ from, to });
+		return;
+	}
+
+	Hanoi(from, to, mid, n - 1);
+	records.push_back({ from, to });
+	Hanoi(mid, from, to, n - 1);
 }
 
 int main() {
-    cin >> n;
-    // 하노이 원판에서 원판의 갯수가 n일때 최소 횟수: 2^n - 1
-    // 1 << n 은 2^n 과 동일 (시프트 연산)
-    cout << (1 << n) - 1 << "\n";
-    Move(n, 1, 3, 2);
+	cin >> n;
+	Hanoi(1, 2, 3, n);
 
-    return 0;
+	cout << records.size() << '\n';
+	for (auto r : records) {
+		cout << r.first << ' ' << r.second << '\n';
+	}
+	return 0;
 }
