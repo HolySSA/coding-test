e6abd4753ca5a68b17ab4193d10242225cc64d22
SSA <158470963+HolySSA@users.noreply.github.com>
2025-03-18 10:32:24 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,57 +1,66 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, complex=1, tmp=0;
-int house[25][25] = {0, };
-bool visited[25][25] = {false, };
-vector<int> houseNum;
-int dy[] = { 0, 0, 1, -1 };
-int dx[] = { 1, -1, 0, 0 };
+int n;
+int map[25][25];
+bool visited[25][25];
+vector<int> answer;
 
-void DFS(int row, int col){
-    visited[row][col] = true;
-    tmp++;
+int dr[] = { 1, 0, -1, 0 };
+int dc[] = { 0, 1, 0, -1 };
 
-    for(int i=0; i<4; i++) {
-        int nx = row + dx[i];
-        int ny = col + dy[i];
-        
-        if(nx < 0 || ny < 0 || nx >= n || ny >= n)
-            continue;
- 
-        if(house[nx][ny] == 1 && !visited[nx][ny]) {
-            DFS(nx, ny);
-        }
-    }
+int House(int r, int c) {
+	queue<pair<int, int>> q;
+	q.push({ r, c });
+	visited[r][c] = true;
+
+	int count = 1;
+	while (!q.empty()) {
+		auto cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nr = cur.first + dr[i];
+			int nc = cur.second + dc[i];
+
+			if (nr < 0 || nc < 0 || nr >= n || nc >= n)
+				continue;
+			if (visited[nr][nc] || map[nr][nc] == 0)
+				continue;
+
+			q.push({ nr, nc });
+			visited[nr][nc] = true;
+			count++;
+		}
+	}
+
+	return count;
 }
 
-int main(){
-    cin>>n;
-    
-    for(int i=0; i<n; i++){
-        string str; cin>>str;
-        for(int j=0; j<n; j++)
-            house[i][j] = str[j] - '0';
-    }
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(!visited[i][j] && house[i][j] == 1){
-                DFS(i, j);
-                complex++;
-                houseNum.push_back(tmp);
-                tmp = 0;
-            }
-        }
-    }
-    
-    sort(houseNum.begin(), houseNum.end());
-    
-    cout<<complex-1<<'\n';
-    for(int i=0; i<houseNum.size(); i++)
-        cout<<houseNum[i]<<'\n';  
-    
-    return 0;
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string str; cin >> str;
+		for (int j = 0; j < n; j++) {
+			map[i][j] = str[j] - '0';
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j] && map[i][j] == 1) {
+				answer.push_back(House(i, j));
+			}
+		}
+	}
+	
+	sort(answer.begin(), answer.end());
+	cout << answer.size() << '\n';
+	for (int a : answer) {
+		cout << a << '\n';
+	}
+	return 0;
 }
