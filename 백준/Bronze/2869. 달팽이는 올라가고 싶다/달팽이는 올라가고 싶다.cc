b354404f9c89694a951a18995e08f307cb3d2af5
SSA <158470963+HolySSA@users.noreply.github.com>
2025-03-19 10:06:34 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int a, b, v, cur = 0, answer = 1;
+
+int main() {
+	cin >> a >> b >> v;
+
+    answer += (v - a) / (a - b);
+
+    // 낮에 도착일 경우 1일 더
+    if ((v - a) % (a - b) != 0)
+        answer++;
+
+    if (a == v)
+        cout << 1;
+    else
+        cout << answer;
+	return 0;
+}
