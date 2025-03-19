6c252a440f8906fe8360de4cb97e9437195987bc
SSA <158470963+HolySSA@users.noreply.github.com>
2025-03-19 09:59:41 +0900

    [Silver II] Title: 나무 자르기, Time: 156 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,44 +1,37 @@
-// 2805 나무 자르기
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int n, m, maxheight = 0;
-vector<int> height;
+int n, m, low = 0, high = 0;
+vector<int> trees;
+
+long long Cut(int height) {
+	long long sum = 0;
+	for (int t : trees) {
+		if (t > height)
+			sum += (t - height);
+	}
+
+	return sum;
+}
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
 	cin >> n >> m;
-	height.resize(n);
+	trees.resize(n);
 	for (int i = 0; i < n; i++) {
-		cin >> height[i];
-		maxheight = max(maxheight, height[i]);
+		cin >> trees[i];
+		high = max(high, trees[i]);
 	}
 
-	int left = 0;
-	int right = maxheight;
+	while (low <= high) {
+		int mid = (low + high) / 2;
 
-	int result = 0;
-
-	while (left <= right) {
-		int mid = (left + right) / 2;
-
-		long long total = 0;
-		for (auto i = 0; i < n; i++) {
-			if (height[i] > mid)
-				total += height[i] - mid;
-		}
-		if (total < m) {
-			right = mid - 1;
-		}
-		else {
-			result = mid;
-			left = mid + 1;
-		}
+		if (Cut(mid) >= m)
+			low = mid + 1;
+		else
+			high = mid - 1;
 	}
-	cout << result;
 
+	cout << high;
 	return 0;
 }
